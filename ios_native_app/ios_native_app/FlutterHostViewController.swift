ea47443f55865bbb8992297252a84773ae4b7ff6
Ludy Su <ludy.su@gmail.com>
2019-05-07 11:48:02 +1200

    Finished flutter integration for iOS

---
+++ ios_native_app/ios_native_app/FlutterHostViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FlutterHostViewController.swift
+//  ios_native_app
+//
+//  Created by Ludy Su on 7/05/19.
+//  Copyright © 2019 orz.ludy.su. All rights reserved.
+//
+
+import UIKit
+
+class FlutterHostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Flutter View"
+    }
+
+}
