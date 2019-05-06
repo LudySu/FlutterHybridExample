804995378c668f021017ade5a05b56d14c07657e
Ludy Su <ludy.su@gmail.com>
2019-05-07 11:23:44 +1200

    Finished iOS native app

---
+++ ios_native_app/ios_native_app/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iOS Native App
+//
+//  Created by Ludy Su on 28/03/19.
+//  Copyright © 2019 Ludy Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func goNextAction(_ sender: Any) {
+    }
+
+}
+
